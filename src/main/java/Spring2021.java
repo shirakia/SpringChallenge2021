4441bcfb31446ed3892c8a07a1df4e96dd309e8f
shirakia <atsuo.shiraki@gmail.com>
2021-05-14 16:45:37 +0900

    Run 100 times

--- src/main/java/Spring2021.java
+++ src/main/java/Spring2021.java
@@ -1,6 +1,6 @@
-import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+import com.codingame.gameengine.runner.simulate.GameResult;
 
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
 
@@ -21,26 +21,35 @@ public class Spring2021 {
     }
 
     public static void launchGame() throws IOException, InterruptedException {
+        Integer win_count = 0;
+        Integer lose_count = 0;
+        Integer draw_count = 0;
+        for (Long seed = 1L; seed <= 100; seed++){
+            MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
+            gameRunner.setLeagueLevel(3);
+            Properties gameParameters = new Properties();
+            gameRunner.setGameParameters(gameParameters);
 
-        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
-        gameRunner.setLeagueLevel(3);
-        Properties gameParameters = new Properties();
-        gameRunner.setGameParameters(gameParameters);
+            gameRunner.addAgent(
+                DEFAULT_AI,
+                "Tororo",
+                "https://static.codingame.com/servlet/fileservlet?id=61910307869345"
+            );
 
-        gameRunner.addAgent(
-            DEFAULT_AI,
-            "Tororo",
-            "https://static.codingame.com/servlet/fileservlet?id=61910307869345"
-        );
-        
-        gameRunner.addAgent(
-            DEFAULT_AI,
-            "Ghilbib",
-            "https://static.codingame.com/servlet/fileservlet?id=61910289640958"
-        );
-        
-        gameRunner.setSeed(7308340236785320085L);
+            gameRunner.addAgent(
+                DEFAULT_AI,
+                "Ghilbib",
+                "https://static.codingame.com/servlet/fileservlet?id=61910289640958"
+            );
 
-        gameRunner.start(8888);
+            gameRunner.setSeed(seed);
+            GameResult result = gameRunner.simulate();
+            Integer my_score = result.scores.get(0);
+            Integer opp_score = result.scores.get(1);
+            if (my_score > opp_score) win_count++;
+            else if (my_score < opp_score) lose_count++;
+            else draw_count++;
+            System.out.println("Game[" + seed + "] Score: " + my_score + ":" + opp_score + "  Total win-lose-draw: " + win_count + "-" + lose_count + "-" + draw_count);
+        }
     }
 }
